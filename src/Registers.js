42be3ab52d9315a47cd9c993ef048d8425208a14
micrus <michele.russo@studenti.unicam.it>
2021-05-29 12:36:36 +0200

    added push and pop from stack, and stack over-under flow assertion

--- src/Registers.js
+++ src/Registers.js
@@ -3,6 +3,7 @@ import { NUMBER_OF_REGISTERS, STACK_SIZE } from "./constants/registersConstants"
 
 export class Registers{
     constructor(){
+        console.log("Create new Registers");
         this.V = new Int8Array(NUMBER_OF_REGISTERS);
         this.I = 0;
         this.delayTimer = 0;
@@ -21,5 +22,26 @@ export class Registers{
         this.PC = LOAD_PROGRAM_ADDRESS;
         this.stack.fill(0);
         this.SP = -1;
+    }
+
+    stackPush(value){
+        this.SP++;
+        this.assertStackOverflow();
+        this.stack[this.SP] = value;
+    }
+
+    stackPop(){
+        const value = this.stack[this.SP];
+        this.SP--;
+        this.assertStackUnderflow();
+        return value;
+    }
+
+    assertStackUnderflow(){
+        console.assert(this.SP>= -1, `Error stack Underflow`);
+    }
+
+    assertStackOverflow(){
+        console.assert(this.SP < STACK_SIZE, `Error stack Overflow`);
     }
 }
